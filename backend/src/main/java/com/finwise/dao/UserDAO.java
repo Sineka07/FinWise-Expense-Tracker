88233da0126aa32dcaf11370020a0bd1b017e203
Sineka07 <snehasenthilkumar91@gmail.com>
2026-09-22 21:56:33 +0530

    Backend updated

--- backend/src/main/java/com/finwise/dao/UserDAO.java
+++ backend/src/main/java/com/finwise/dao/UserDAO.java
@@ -1,9 +1,9 @@
 package com.finwise.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,23 +13,25 @@ import com.finwise.model.User;
 @Transactional
 public class UserDAO {
     
-    @Autowired
-    private SessionFactory sessionFactory;
+    @PersistenceContext
+    private EntityManager entityManager;
     
     public void saveUser(User user) {
-        Session session = sessionFactory.getCurrentSession();
-        session.saveOrUpdate(user);
+        if (user.getId() == null) {
+            entityManager.persist(user);
+        } else {
+            entityManager.merge(user);
+        }
     }
     
     public User getUserById(Long id) {
-        Session session = sessionFactory.getCurrentSession();
-        return session.get(User.class, id);
+        return entityManager.find(User.class, id);
     }
     
     public User getUserByEmail(String email) {
-        Session session = sessionFactory.getCurrentSession();
-        Query<User> query = session.createQuery("FROM User WHERE email = :email", User.class);
+        TypedQuery<User> query = entityManager.createQuery(
+            "FROM User WHERE email = :email", User.class);
         query.setParameter("email", email);
-        return query.uniqueResult();
+        return query.getResultStream().findFirst().orElse(null);
     }
 }
